722d469a6f28d59ba1fa58b2dc3625250f6757eb
FloresTM <seb.flores2002@gmail.com>
2024-08-30 00:53:26 +1000

    adding router and shadcn

---
+++ platinum-web/src/Test.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+type Props = {};
+
+const Test = (props: Props) => {
+  return (
+    <div>
+      <Button variant="default">Test</Button>
+    </div>
+  );
+};
+
+export default Test;
--- platinum-web/src/main.tsx
+++ platinum-web/src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Test from "./Test";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/test",
+    element: <Test />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
