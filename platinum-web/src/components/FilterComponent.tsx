2804473ad84a110fd43b5aff574ee418f1fc10be
FloresTM <seb.flores2002@gmail.com>
2024-08-31 17:35:46 +1000

    filters work

---
+++ platinum-web/src/components/FilterComponent.tsx
@@ -0,0 +1,102 @@
+import { Check, ChevronDown } from "lucide-react";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "../components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "../components/ui/popover";
+import { Button } from "./ui/button";
+import { cn } from "../lib/utils";
+import { Category, CategoryAtributes } from "../models/category";
+
+type FilterComponentProps = {
+  attribute: CategoryAtributes;
+  categories: Category[];
+  open: boolean;
+  selectedValue: string;
+  enabled: boolean;
+  onToggleOpen: (open: boolean) => void;
+  onSelect: (name: string) => void;
+  onReset: () => void;
+};
+
+const FilterComponent = ({
+  attribute,
+  categories,
+  open,
+  selectedValue,
+  enabled,
+  onToggleOpen,
+  onSelect,
+  onReset,
+}: FilterComponentProps) => {
+  return (
+    <Popover open={open} onOpenChange={onToggleOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="ghost"
+          role="combobox"
+          aria-expanded={open}
+          disabled={!enabled}
+          className="w-[300px] py-8 justify-between text-[#545454] bg-white shadow-sm"
+          onClick={onReset}
+        >
+          {selectedValue ? (
+            <div>
+              <h4 className="font-bold text-left mb-1">{attribute.name}</h4>
+              {selectedValue}
+            </div>
+          ) : (
+            <div className="text-left">
+              <h4 className="font-bold">{attribute.name}</h4>
+              <p className="font-light text-[15px] mt-1">
+                Seleccionar {attribute.name.toLowerCase()}
+              </p>
+            </div>
+          )}
+          <ChevronDown className="ml-2 h-4 w-4 shrink-0 opacity-100 text-naranja" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[300px] p-0">
+        <Command>
+          <CommandInput
+            placeholder={`Buscar ${attribute.name.toLowerCase()}...`}
+          />
+          <CommandList>
+            <CommandEmpty>
+              No se encontró {attribute.name.toLowerCase()}.
+            </CommandEmpty>
+            <CommandGroup>
+              {categories.map((category) => (
+                <CommandItem
+                  key={category.id}
+                  value={category.name}
+                  onSelect={() => onSelect(category.name)}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      selectedValue === category.name
+                        ? "opacity-100"
+                        : "opacity-0"
+                    )}
+                  />
+                  {category.name}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default FilterComponent;
--- platinum-web/src/components/FilterSection.tsx
+++ platinum-web/src/components/FilterSection.tsx
@@ -1,441 +1,90 @@
-import { Check, ChevronDown } from "lucide-react";
-
-// import {
-//   Select,
-//   SelectContent,
-//   SelectGroup,
-//   SelectItem,
-//   SelectLabel,
-//   SelectTrigger,
-//   SelectValue,
-// } from "../components/ui/select";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "../components/ui/command";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "../components/ui/popover";
 import { Category } from "../models/category";
-import { Button } from "./ui/button";
 import { useState } from "react";
-import { cn } from "../lib/utils";
+import { useCategories } from "../hooks/useCategories";
+import FilterComponent from "./FilterComponent";
 
 type FilterSectionProps = {
   categories: Category[];
 };
 
 const FilterSection = ({ categories }: FilterSectionProps) => {
-  const [open, setOpen] = useState(false);
-  const [selectedCategory, setSelectedCategory] = useState<
-    string | undefined
-  >();
+  const { category } = useCategories();
 
-  const handleSelect = (name: string) => {
-    const selected = categories.find((category) => category.name === name);
-    setSelectedCategory(selected?.id);
-    setOpen(false);
+  const [attributeStates, setAttributeStates] = useState(() =>
+    category.attributes?.reduce((acc, attribute, index) => {
+      acc[attribute.id] = {
+        open: false,
+        selectedValue: "",
+        disabled: index !== 0,
+      };
+      return acc;
+    }, {} as { [key: string]: { open: boolean; selectedValue: string; disabled: boolean } })
+  );
+
+  const handleSelect = (attributeId: string, name: string) => {
+    setAttributeStates((prevState: any) => {
+      const attributeKeys = Object.keys(prevState);
+      const currentIndex = attributeKeys.indexOf(attributeId);
+
+      const updatedState = {
+        ...prevState,
+        [attributeId]: {
+          ...prevState![attributeId],
+          selectedValue: name,
+          open: false,
+        },
+      };
+
+      if (currentIndex + 1 < attributeKeys.length) {
+        const nextId = attributeKeys[currentIndex + 1];
+        updatedState[nextId].disabled = false;
+      }
+
+      return updatedState;
+    });
+  };
+
+  const handleResetFrom = (attributeId: string) => {
+    setAttributeStates((prevState: any) => {
+      const attributeKeys = Object.keys(prevState);
+      const currentIndex = attributeKeys.indexOf(attributeId);
+
+      const updatedState = { ...prevState };
+      for (let i = currentIndex + 1; i < attributeKeys.length; i++) {
+        const id = attributeKeys[i];
+        updatedState[id] = {
+          ...updatedState[id],
+          selectedValue: "",
+          disabled: true,
+        };
+      }
+
+      return updatedState;
+    });
+  };
+
+  const toggleOpen = (attributeId: string, open: boolean) => {
+    setAttributeStates((prevState) => ({
+      ...prevState,
+      [attributeId]: { ...prevState![attributeId], open },
+    }));
   };
 
   return (
-    <div className="w-full flex gap-4">
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="ghost"
-            role="combobox"
-            aria-expanded={open}
-            className="w-[300px] py-8 justify-between text-[#545454] bg-white shadow-sm"
-          >
-            {selectedCategory ? (
-              <div>
-                <h4 className="font-bold text-left mb-1">Marca</h4>
-                {
-                  categories.find(
-                    (category) => category.id === selectedCategory
-                  )?.name
-                }
-              </div>
-            ) : (
-              <div className="text-left">
-                <h4 className="font-bold">Marca</h4>
-                <p className="font-light text-[15px] mt-1">
-                  Seleccionar la marca
-                </p>
-              </div>
-            )}
-            <ChevronDown className="ml-2 h-4 w-4 shrink-0 opacity-100 text-naranja" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[300px] p-0">
-          <Command>
-            <CommandInput placeholder="Buscar marca..." />
-            <CommandList>
-              <CommandEmpty>No se encontró la marca.</CommandEmpty>
-              <CommandGroup>
-                {categories.map((category) => (
-                  <CommandItem
-                    key={category.id}
-                    value={category.name}
-                    onSelect={handleSelect}
-                  >
-                    <Check
-                      className={cn(
-                        "mr-2 h-4 w-4",
-                        selectedCategory === category.id
-                          ? "opacity-100"
-                          : "opacity-0"
-                      )}
-                    />
-                    {category.name}
-                  </CommandItem>
-                ))}
-              </CommandGroup>
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </Popover>
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="ghost"
-            role="combobox"
-            aria-expanded={open}
-            className="w-[300px] py-8 justify-between text-[#545454] bg-white shadow-sm"
-          >
-            {selectedCategory ? (
-              <div>
-                <h4 className="font-bold text-left mb-1">Marca</h4>
-                {
-                  categories.find(
-                    (category) => category.id === selectedCategory
-                  )?.name
-                }
-              </div>
-            ) : (
-              <div className="text-left">
-                <h4 className="font-bold">Marca</h4>
-                <p className="font-light text-[15px] mt-1">
-                  Seleccionar la marca
-                </p>
-              </div>
-            )}
-            <ChevronDown className="ml-2 h-4 w-4 shrink-0 opacity-100 text-naranja" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[300px] p-0">
-          <Command>
-            <CommandInput placeholder="Buscar marca..." />
-            <CommandList>
-              <CommandEmpty>No se encontró la marca.</CommandEmpty>
-              <CommandGroup>
-                {categories.map((category) => (
-                  <CommandItem
-                    key={category.id}
-                    value={category.name}
-                    onSelect={handleSelect}
-                  >
-                    <Check
-                      className={cn(
-                        "mr-2 h-4 w-4",
-                        selectedCategory === category.id
-                          ? "opacity-100"
-                          : "opacity-0"
-                      )}
-                    />
-                    {category.name}
-                  </CommandItem>
-                ))}
-              </CommandGroup>
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </Popover>
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="ghost"
-            role="combobox"
-            aria-expanded={open}
-            className="w-[300px] py-8 justify-between text-[#545454] bg-white shadow-sm"
-          >
-            {selectedCategory ? (
-              <div>
-                <h4 className="font-bold text-left mb-1">Marca</h4>
-                {
-                  categories.find(
-                    (category) => category.id === selectedCategory
-                  )?.name
-                }
-              </div>
-            ) : (
-              <div className="text-left">
-                <h4 className="font-bold">Marca</h4>
-                <p className="font-light text-[15px] mt-1">
-                  Seleccionar la marca
-                </p>
-              </div>
-            )}
-            <ChevronDown className="ml-2 h-4 w-4 shrink-0 opacity-100 text-naranja" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[300px] p-0">
-          <Command>
-            <CommandInput placeholder="Buscar marca..." />
-            <CommandList>
-              <CommandEmpty>No se encontró la marca.</CommandEmpty>
-              <CommandGroup>
-                {categories.map((category) => (
-                  <CommandItem
-                    key={category.id}
-                    value={category.name}
-                    onSelect={handleSelect}
-                  >
-                    <Check
-                      className={cn(
-                        "mr-2 h-4 w-4",
-                        selectedCategory === category.id
-                          ? "opacity-100"
-                          : "opacity-0"
-                      )}
-                    />
-                    {category.name}
-                  </CommandItem>
-                ))}
-              </CommandGroup>
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </Popover>
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="ghost"
-            role="combobox"
-            aria-expanded={open}
-            className="w-[300px] py-8 justify-between text-[#545454] bg-white shadow-sm"
-          >
-            {selectedCategory ? (
-              <div>
-                <h4 className="font-bold text-left mb-1">Marca</h4>
-                {
-                  categories.find(
-                    (category) => category.id === selectedCategory
-                  )?.name
-                }
-              </div>
-            ) : (
-              <div className="text-left">
-                <h4 className="font-bold">Marca</h4>
-                <p className="font-light text-[15px] mt-1">
-                  Seleccionar la marca
-                </p>
-              </div>
-            )}
-            <ChevronDown className="ml-2 h-4 w-4 shrink-0 opacity-100 text-naranja" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[300px] p-0">
-          <Command>
-            <CommandInput placeholder="Buscar marca..." />
-            <CommandList>
-              <CommandEmpty>No se encontró la marca.</CommandEmpty>
-              <CommandGroup>
-                {categories.map((category) => (
-                  <CommandItem
-                    key={category.id}
-                    value={category.name}
-                    onSelect={handleSelect}
-                  >
-                    <Check
-                      className={cn(
-                        "mr-2 h-4 w-4",
-                        selectedCategory === category.id
-                          ? "opacity-100"
-                          : "opacity-0"
-                      )}
-                    />
-                    {category.name}
-                  </CommandItem>
-                ))}
-              </CommandGroup>
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </Popover>
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="ghost"
-            role="combobox"
-            aria-expanded={open}
-            className="w-[300px] py-8 justify-between text-[#545454] bg-white shadow-sm"
-          >
-            {selectedCategory ? (
-              <div>
-                <h4 className="font-bold text-left mb-1">Marca</h4>
-                {
-                  categories.find(
-                    (category) => category.id === selectedCategory
-                  )?.name
-                }
-              </div>
-            ) : (
-              <div className="text-left">
-                <h4 className="font-bold">Marca</h4>
-                <p className="font-light text-[15px] mt-1">
-                  Seleccionar la marca
-                </p>
-              </div>
-            )}
-            <ChevronDown className="ml-2 h-4 w-4 shrink-0 opacity-100 text-naranja" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[300px] p-0">
-          <Command>
-            <CommandInput placeholder="Buscar marca..." />
-            <CommandList>
-              <CommandEmpty>No se encontró la marca.</CommandEmpty>
-              <CommandGroup>
-                {categories.map((category) => (
-                  <CommandItem
-                    key={category.id}
-                    value={category.name}
-                    onSelect={handleSelect}
-                  >
-                    <Check
-                      className={cn(
-                        "mr-2 h-4 w-4",
-                        selectedCategory === category.id
-                          ? "opacity-100"
-                          : "opacity-0"
-                      )}
-                    />
-                    {category.name}
-                  </CommandItem>
-                ))}
-              </CommandGroup>
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </Popover>
-
-      {/* <Select>
-        <SelectTrigger className="h-full">
-          {categories.length > 0 ? (
-            <div className="text-left">
-              <h4 className="font-bold">Marca</h4>
-              <p className="text-[#545454]">Seleccionar la marca</p>
-            </div>
-          ) : (
-            <SelectValue placeholder="Platinum Driveline" />
-          )}
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Categorías</SelectLabel>
-            {categories.map((category) => (
-              <SelectItem key={category.id} value={category.id}>
-                {category.name}
-              </SelectItem>
-            ))}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-      <Select>
-        <SelectTrigger className="h-full">
-          {categories.length > 0 ? (
-            <div className="text-left">
-              <h4 className="font-bold">Modelo</h4>
-              <p className="text-[#545454]">Seleccionar el modelo</p>
-            </div>
-          ) : (
-            <SelectValue placeholder="Platinum Driveline" />
-          )}
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Categorías</SelectLabel>
-            {categories.map((category) => (
-              <SelectItem key={category.id} value={category.id}>
-                {category.name}
-              </SelectItem>
-            ))}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-      <Select>
-        <SelectTrigger className="h-full">
-          {categories.length > 0 ? (
-            <div className="text-left">
-              <h4 className="font-bold">Tipo</h4>
-              <p className="text-[#545454]">Seleccionar el Tipo</p>
-            </div>
-          ) : (
-            <SelectValue placeholder="Platinum Driveline" />
-          )}
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Categorías</SelectLabel>
-            {categories.map((category) => (
-              <SelectItem key={category.id} value={category.id}>
-                {category.name}
-              </SelectItem>
-            ))}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-      <Select>
-        <SelectTrigger className="h-full">
-          {categories.length > 0 ? (
-            <div className="text-left">
-              <h4 className="font-bold">Motor</h4>
-              <p className="text-[#545454]">Seleccionar motor</p>
-            </div>
-          ) : (
-            <SelectValue placeholder="Platinum Driveline" />
-          )}
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Categorías</SelectLabel>
-            {categories.map((category) => (
-              <SelectItem key={category.id} value={category.id}>
-                {category.name}
-              </SelectItem>
-            ))}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-      <Select>
-        <SelectTrigger className="h-full">
-          {categories.length > 0 ? (
-            <div className="text-left">
-              <h4 className="font-bold">Año</h4>
-              <p className="text-[#545454]">Seleccionar año</p>
-            </div>
-          ) : (
-            <SelectValue placeholder="Platinum Driveline" />
-          )}
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Categorías</SelectLabel>
-            {categories.map((category) => (
-              <SelectItem key={category.id} value={category.id}>
-                {category.name}
-              </SelectItem>
-            ))}
-          </SelectGroup>
-        </SelectContent>
-      </Select> */}
+    <div className="flex gap-4">
+      {category.attributes?.map((attribute) => (
+        <FilterComponent
+          key={attribute.id}
+          attribute={attribute}
+          categories={categories}
+          open={attributeStates![attribute.id]?.open}
+          selectedValue={attributeStates![attribute.id]?.selectedValue}
+          enabled={!attributeStates![attribute.id]?.disabled}
+          onToggleOpen={(open: boolean) => toggleOpen(attribute.id, open)}
+          onSelect={(name: string) => handleSelect(attribute.id, name)}
+          onReset={() => handleResetFrom(attribute.id)}
+        />
+      ))}
     </div>
   );
 };
